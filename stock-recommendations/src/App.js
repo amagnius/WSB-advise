a876bb8d1a05e704b29a48fd60a9fe3a6f89e850
Jones Mella <Jonesma@kth.se>
2023-04-28 19:37:29 +0200

    Modify react app to display buy and sell lists

--- stock-recommendations/src/App.js
+++ stock-recommendations/src/App.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Stock from './Stock';
+import StockList from './StockList';
 
 const App = () => {
   const [recommendations, setRecommendations] = useState([]);
@@ -23,23 +23,25 @@ const App = () => {
     }
   };
 
+  const buyRecommendations = recommendations.filter(
+    stock => stock.action.toLowerCase() === 'buy'
+  );
+
+  const sellRecommendations = recommendations.filter(
+    stock => stock.action.toLowerCase() === 'sell'
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Today's Stock Recommendations</h1>
       </header>
-      <main className="stocks-container">
-        {recommendations.map(stock => (
-          <Stock
-            key={stock.symbol}
-            name={stock.name}
-            symbol={stock.symbol}
-            action={stock.action}
-          />
-        ))}
+      <main className="stock-lists">
+        <StockList title="Buy" stocks={buyRecommendations} />
+        <StockList title="Sell" stocks={sellRecommendations} />
       </main>
     </div>
   );
-}
+};
 
 export default App;
