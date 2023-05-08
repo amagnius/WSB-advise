942f73095a1f74cd76f2250dfeca0a81352a2966
Axel Magnius <amagnius@kth.se>
2023-05-08 15:56:46 +0200

    Implement getComments method

--- flask_app/comment_analysis.py
+++ flask_app/comment_analysis.py
@@ -5,3 +5,13 @@ reddit = praw.Reddit(
     client_secret="m735OX5eD595ZhxYom1w5RssVyjUyg",
     user_agent="website:https://amagnius.github.io/WSB-advise/:v1 (by u/Dazzling-Pollution-6)",
 )
+
+def getComments(subreddit, numComments):
+    # Create an instance of the subreddit
+    subreddit = reddit.subreddit(subreddit)
+
+    # Read in latest comments to a list
+    recent_comments = subreddit.comments(limit=numComments)
+
+    return recent_comments
+
