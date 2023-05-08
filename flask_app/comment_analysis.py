44d95251205d253bd873b7db0d265426833d6675
Axel Magnius <amagnius@kth.se>
2023-05-08 14:36:37 +0200

    Set up Reddit API authentication

---
+++ flask_app/comment_analysis.py
@@ -0,0 +1,7 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="ca67SLBHFy2Am7kPZ-TdKA",
+    client_secret="m735OX5eD595ZhxYom1w5RssVyjUyg",
+    user_agent="website:https://amagnius.github.io/WSB-advise/:v1 (by u/Dazzling-Pollution-6)",
+)
