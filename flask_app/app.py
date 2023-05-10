7e48db24cace3932f41553aab6d2edbf446850cb
Jones Mella <Jonesma@kth.se>
2023-05-10 14:31:49 +0200

    Create backend api endpoint

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from comment_analysis import fetch_stock_recommendations
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/api/stock_recommendations')
+def stock_recommendations():
+    recommendations = fetch_stock_recommendations()
+    return jsonify(recommendations)
 
 if __name__ == '__main__':
     app.run()
