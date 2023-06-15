841e0abe9b4b1fe4e0065d3c6a7aa38ce24a3731
JonesMea <114910882+JonesMea@users.noreply.github.com>
2023-06-15 13:56:09 +0200

    Remove client_id and client_secret from public repo

--- flask_app/comment_analysis.py
+++ flask_app/comment_analysis.py
@@ -2,8 +2,6 @@ import praw
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 reddit = praw.Reddit(
-    client_id="ca67SLBHFy2Am7kPZ-TdKA",
-    client_secret="m735OX5eD595ZhxYom1w5RssVyjUyg",
     user_agent="website:https://amagnius.github.io/WSB-advise/:v1 (by u/Dazzling-Pollution-6)",
 )
 
